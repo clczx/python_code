46f1ef5f00daa0f4a3535c2be7adb2855c6f1a59
chenzhongxiang <chenzhongxiang@chenzhongxiangdeMacBook-Pro.local>
2023-04-09 22:38:51 +0800

    init

---
+++ gen_puzzle.py
@@ -0,0 +1,105 @@
+# 定义数独棋盘
+# 定义数独难度
+difficulty = "easy"
+
+# 初始化数独棋盘
+def gen_puzz():
+    board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    # 根据难度设置空白格数量
+    if difficulty == "easy":
+        blank_count = 30
+    elif difficulty == "medium":
+        blank_count = 40
+    elif difficulty == "hard":
+        blank_count = 50
+    else:
+        blank_count = 30
+    # 随机生成数独棋盘
+    generate_board(board)
+    # 随机挖空格
+    dig_holes(blank_count, board)
+    print(board)
+    return board
+
+# 随机生成数独棋盘
+def generate_board(board):
+    import random
+    # 定义数字范围
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # 随机填充第一行
+    random.shuffle(nums)
+    for i in range(9):
+        board[0][i] = nums[i]
+    # 递归填充数独棋盘
+    fill_board(0, 0, board)
+
+
+# 递归填充数独棋盘
+def fill_board(row, col, board):
+    import random
+    # 判断是否填充完成
+    if row == 8 and col == 9:
+        return True
+    # 判断是否填充完一行
+    if col == 9:
+        row += 1
+        col = 0
+    # 判断当前位置是否已填充
+    if board[row][col] > 0:
+        return fill_board(row, col + 1, board)
+    # 随机填充数字
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    random.shuffle(nums)
+    for num in nums:
+        if is_valid(row, col, num, board):
+            board[row][col] = num
+            if fill_board(row, col + 1, board):
+                return True
+            board[row][col] = 0
+    return False
+
+# 判断填充数字是否合法
+def is_valid(row, col, num, board):
+    # 判断行是否合法
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+    # 判断列是否合法
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    # 判断宫是否合法
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+    return True
+
+
+def dig_holes(blank_count, board):
+    import random
+    # 随机挖空格
+    for i in range(blank_count):
+        row = random.randint(0, 8)
+        col = random.randint(0, 8)
+        # 判断当前位置是否已挖空
+        while board[row][col] == 0:
+            row = random.randint(0, 8)
+            col = random.randint(0, 8)
+        # 挖空当前位置
+        board[row][col] = 0
+
+
+gen_puzz()
---
+++ sudo.py
@@ -0,0 +1,206 @@
+import turtle
+import gen_puzzle
+
+class SudokuGame:
+    def __init__(self):
+        self.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        self.generated = 81 * [False]
+        self.turtles = {}
+
+        self.screen = turtle.Screen()
+        self.screen.setup(500, 500)
+        self.screen.title("数独游戏")
+        self.screen.bgcolor("#ffffff")
+
+        self.board_size = 450
+        self.cell_size = self.board_size / 9
+
+        self.pen = turtle.Turtle()
+
+
+    def draw_board(self):
+        left_min, left_up = -self.board_size / 2 -5 ,  self.board_size / 2 + 5
+        board_size = self.board_size
+        cell_size = self.cell_size
+
+        self.pen.speed(0)
+        self.pen.pensize(3)
+        self.pen.penup()
+
+        self.pen.goto(left_min, left_up)
+        self.pen.pendown()
+        for i in range(4):
+            self.pen.forward(board_size)
+            self.pen.right(90)
+            
+        # 绘制横线
+        for i in range(1, 9):
+            self.pen.penup()
+            self.pen.goto(left_min, left_up - i * cell_size)
+            self.pen.pendown()
+            self.pen.forward(board_size)
+        
+        # 绘制竖线
+        self.pen.right(90)
+        for i in range(1, 9):
+            self.pen.penup()
+            self.pen.goto(left_min + i * cell_size, left_up)
+            self.pen.pendown()
+            self.pen.forward(board_size)
+        
+        # 绘制粗线
+        self.pen.pensize(5)
+        for i in range(1, 3):
+            self.pen.penup()
+            self.pen.goto(left_min + i * cell_size * 3, left_up)
+            self.pen.pendown()
+            self.pen.forward(board_size)
+            
+        self.pen.left(90)
+        for i in range(1, 3):
+            self.pen.penup()
+            self.pen.goto(left_min, left_up - i * cell_size * 3)
+            self.pen.pendown()
+            self.pen.forward(board_size)
+     
+        for i in range(9):
+            for j in range(9):
+                if self.board[i][j] != 0:
+                    self.pen.penup()
+                    y = left_up - i * cell_size - cell_size * 3 / 4
+                    x = left_min + j * cell_size + cell_size / 2
+                    self.pen.goto(x, y)
+                    self.pen.write(str(self.board[i][j]), align='center', font=('Arial', 20, 'normal'))
+
+        self.pen.hideturtle()
+
+    def is_valid(self, row, col, num):
+        # 判断行是否合法
+        for i in range(9):
+            if self.board[row][i] == num:
+                return False
+        # 判断列是否合法
+        for i in range(9):
+            if self.board[i][col] == num:
+                return False
+        # 判断宫是否合法
+        start_row = (row // 3) * 3
+        start_col = (col // 3) * 3
+        for i in range(3):
+            for j in range(3):
+                if self.board[start_row + i][start_col + j] == num:
+                    return False
+        return True
+
+
+    def handle_click(self, x, y):
+        left_min, left_up = -self.board_size / 2 -5 ,  self.board_size / 2 + 5
+        if left_min < x < left_min + self.board_size and -self.board_size + left_up< y < left_up:
+            # 将坐标转换成矩阵索引
+            row = int((left_up - y) // self.cell_size)
+            col = int((x - left_min) // self.cell_size)
+            print(row, col)
+
+            # 标记选中格子
+#            self.pen.speed(0)
+#            self.pen.pencolor("#000000")
+#            self.pen.penup()
+#            self.pen.goto(left_min + col * 30, left_up - row * 30)
+#            self.pen.pendown()
+#            self.pen.goto(left_min + (col + 1) * 30, left_up - row * 30)
+#            self.pen.goto(left_min + (col + 1) * 30, left_up - (row + 1) * 30)
+#            self.pen.goto(left_min + col * 30, left_up - (row + 1) * 30)
+#            self.pen.goto(left_min + col * 30, left_up - row * 30)
+#
+            # 处理数字输入
+            value = self.screen.numinput("数独游戏", "请输入数字(1-9):", minval=1, maxval=9)
+            while value is not None and not self.is_valid(row, col, value): 
+                value = self.screen.numinput("数独游戏", "输入的数字不符合规则，请重新输入", minval=1,maxval=9)
+                value = int(value) if value is not None else None
+            print(value)
+
+            key = row * 9 + col
+            if value is not None and not self.generated[key]:
+                self.board[row][col] = value
+                y = left_up -  row * self.cell_size - self.cell_size * 3 / 4
+                x = left_min + col * self.cell_size + self.cell_size / 2
+
+                t = self.turtles[key] if key in self.turtles else turtle.Turtle()
+                t.hideturtle()
+                t.penup()
+                t.clear()
+
+                t.color("green")
+                t.goto(x, y)
+                t.write(str(self.board[row][col]), align='center', font=('Arial', 20, 'normal'))
+                self.turtles[key] = t
+
+                if self.is_board_full():
+                    t.hideturtle()
+                    t = turtle.Turtle()
+                    t.color("red")
+                    t.write("Congratulations! You solved the Sudoku puzzle!", align="center", font=("Arial", 20, "normal"))
+            
+
+    def is_board_full(self):
+        for i in range(9):
+            for j in range(9):
+                if self.board[i][j] == 0:
+                    return False
+        return True
+
+    def generate_puzzle(self):
+        a =  [
+    [0, 0, 0, 2, 6, 0, 7, 0, 1],
+    [6, 8, 0, 0, 7, 0, 0, 9, 0],
+    [1, 9, 0, 0, 0, 4, 5, 0, 0],
+    [8, 2, 0, 1, 0, 0, 0, 4, 0],
+    [0, 0, 4, 6, 0, 2, 9, 0, 0],
+    [0, 5, 0, 0, 0, 3, 0, 2, 8],
+    [0, 0, 9, 3, 0, 0, 0, 7, 4],
+    [0, 4, 0, 0, 5, 0, 0, 3, 6],
+    [7, 0, 3, 0, 1, 8, 0, 0, 0]
+]
+        board = gen_puzzle.gen_puzz()
+        print(board)
+        return board
+
+    def init_board(self):
+        # 随机生成一个数独谜题
+        puzzle = self.generate_puzzle()
+
+        # 初始化棋盘
+        self.board = [[0] * 9 for _ in range(9)]
+
+        # 复制数独谜题到棋盘
+        for i in range(9):
+            for j in range(9):
+                self.board[i][j] = puzzle[i][j]             
+                self.generated[i * 9 + j] = True if self.board[i][j] > 0 else False
+
+
+    def start_game(self):
+        # 初始化棋盘
+        self.init_board()
+
+        # 绘制棋盘
+        self.draw_board()
+
+        # 注册点击事件处理函数
+        turtle.onscreenclick(self.handle_click)
+
+        # 进入主循环
+        turtle.mainloop()
+
+game = SudokuGame()
+game.start_game()
